dddd8da8c34750c98e826a110618f626ebc31b9e
TannerFindlay <tannerf978@gmail.com>
2024-11-20 21:09:53 -0800

    Update pollingpt1.c

--- Lab 4/pollingpt1.c
+++ Lab 4/pollingpt1.c
@@ -36,52 +36,42 @@ int polling(void)
         RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
 
         //configure the PA0 (corresponding to LED) as the output pin
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;                                //Enable the ouput to GPIO pin 0
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;                                //Enable the ouput to GPIO pin PA0
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;                            //Set mode to ouput, initialize pin
         GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
         GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-        GPIO_Init(GPIOA, &GPIO_InitStructure);                                    //Enable the output to GPIO pin A (0)
+        GPIO_Init(GPIOA, &GPIO_InitStructure);                                    
 
         //configure PA5 (corresponding to btn 2) as the input pin
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;                                //Enable the input to GPIO pin PA5
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;                             //Set mode to IN, initialize input pin
         GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
         GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
         GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-        while (1)
-        {
-                 if( BUTTON_PUSHED == ReadButton2Status() )
-                {
+        while (1){
+                 if( BUTTON_PUSHED == ReadButton2Status() ) {                      //If button is pushed, turn on LED
                                 GPIO_SetBits(GPIOA, GPIO_Pin_0);
-                }
-                else
-                {
-                                GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+                }else{
+                                GPIO_ResetBits(GPIOA, GPIO_Pin_0);                //If button is not pushed, turn off LED
                 }
         }
-
         return 0;
 }
 
 /**********************************************************/
-int ReadButton2Status( void )
-{
-        if(GPIOA->IDR & GPIO_Pin_5 )
-        {
+int ReadButton2Status( void ){
+        if(GPIOA->IDR & GPIO_Pin_5 ){                                              //If the button is pushed, return "BUTTON_PUSHED" to be used for polling 
                 return( BUTTON_PUSHED );
         }
-
         return( BUTTON_RELEASED );
 }
-
 /**********************************************************/
 
-int main(int argc, char* argv[])
-{
-        polling();
+int main(int argc, char* argv[]){
+        polling();                                                                //Call polling function to turn LED on and off with buttons
         return 0;
 }
 
